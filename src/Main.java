134db41e4efcfcec8ac84d1e08fdc52ce508a4b0
hudafaisal <hudafaisal354@gmail.com>
2026-07-22 16:26:45 +0500

    Writing code in each class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+
+        System.out.println(
+                "SYSTEM VULNERABILITY CHECKLIST"
+        );
+
+
+        ArrayList<Vulnerability> findings =
+                new ArrayList<>();
+
+
+        SystemScanner system =
+                new SystemScanner();
+
+
+        UserScanner users =
+                new UserScanner();
+
+
+        FirewallScanner firewall =
+                new FirewallScanner();
+
+
+        UpdateScanner updates =
+                new UpdateScanner();
+
+
+        EncryptionScanner encryption =
+                new EncryptionScanner();
+
+
+
+        findings.addAll(
+                system.scanSystem()
+        );
+
+
+        findings.addAll(
+                users.checkUsers()
+        );
+
+
+        findings.addAll(
+                firewall.checkFirewall()
+        );
+
+
+        findings.addAll(
+                updates.checkUpdates()
+        );
+
+
+        findings.addAll(
+                encryption.checkEncryption()
+        );
+
+
+
+        ReportGenerator report =
+                new ReportGenerator();
+
+
+        report.generate(
+                findings
+        );
+
+
+        System.out.println(
+                "Security Audit Completed"
+        );
+
+
     }
+
 }
--- src/ReportGenerator.java
+++ src/ReportGenerator.java
@@ -1,2 +1,66 @@
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+
 public class ReportGenerator {
+
+
+
+    public void generate(
+            ArrayList<Vulnerability> vulnerabilities
+    ){
+
+
+        try{
+
+
+            FileWriter writer =
+                    new FileWriter(
+                            "Vulnerability_Report.txt"
+                    );
+
+
+            writer.write(
+                    "SYSTEM VULNERABILITY REPORT\n\n"
+            );
+
+
+            for(
+                    Vulnerability v : vulnerabilities
+            ){
+
+
+                writer.write(
+                        v.toString()
+                );
+
+
+            }
+
+
+            writer.close();
+
+
+            System.out.println(
+                    "Report Generated Successfully"
+            );
+
+
+        }
+
+
+        catch(Exception e){
+
+
+            System.out.println(
+                    "Report Error"
+            );
+
+
+        }
+
+
+    }
+
+
 }
